4f509cf704241eda073298bc824bfcf21d5dcc69
Gabriel Rudan <gabrielrudam92@gmail.com>
2022-04-17 19:27:34 -0300

    Questão 1

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  contador = {
+    valor: 0
+  }
+
+  contarClick = () => {
+    this.setState({
+      valor: this.contador.valor++
+    })
+    console.log("Você pressionou o botão!")
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 20, padding: 20, color: 'grey'}}>Você pressionou o botão:</Text>
+        <Text style={{ fontSize: 60, marginBottom: -20, color: 'blue'}}>{this.contador.valor}</Text>
+        <Text style={{ fontSize: 20, padding: 20, color: 'grey'}}>vez(es)</Text>
+        <StatusBar style="auto" />
+        <View style={{ flexDirection: 'row'}}>
+          <Button onPress={this.contarClick} title="Clique Aqui"></Button>
+        </View>
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
